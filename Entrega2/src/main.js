3964b521f78e282f6970234dd8bdabde3c7fe633
Tomás Godoy <tomas.godoy.litio@hotmail.com>
2025-12-15 20:00:46 -0300

    delete entrega2, tp en entrega1

--- Entrega2/src/main.js
+++
@@ -1,6 +0,0 @@
-// Express
-import express from 'express'
-const SERVER = express();
-const PORT = 8080;
-
-SERVER.use(express.json());
--- Entrega2/src/routes/productos.js
+++
@@ -1,5 +0,0 @@
-// Express
-import express from 'express'
-const SERVER = express();
-const PORT = 8080;
-
